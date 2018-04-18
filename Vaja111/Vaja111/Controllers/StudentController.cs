556faa730f4f11d411347073aca94a2ae89e44f3
student <aljazmaffi@gmail.com>
2018-04-18 11:45:16 +0200

    Updateeeeeee

---
+++ Vaja111/Vaja111/Controllers/StudentController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vaja111.Models;
+
+namespace Vaja111.Controllers
+{
+    public class StudentController : Controller
+    {
+        List<Student> studentList = new List<Student>
+            {
+                new Student() { StudentID = 1, StudentName = "John", Age = 18 } ,
+                new Student() { StudentID = 2, StudentName = "Johnny", Age = 19 } ,
+                new Student() { StudentID = 3, StudentName = "Johnney", Age = 20 } ,
+                new Student() { StudentID = 4, StudentName = "Johnneyson", Age = 21 }
+            };
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(studentList);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var student = studentList.Where(s => s.StudentID == id).FirstOrDefault();
+            return View(student);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student std)
+        {
+            if (ModelState.IsValid)
+            {
+                // posodobi podatke
+                return RedirectToAction("Index");
+            }
+            return View(std);
+        }
+    }
+}
---
+++ Vaja111/Vaja111/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vaja111.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        [Display(Name="Ime")]
+        [Required(ErrorMessage = "Nvjm")]
+        public string StudentName { get; set; }
+        [Display(Name = "Starost")]
+        [Range(18,50,ErrorMessage = "Napačna starost")]
+        public int Age { get; set; }
+    }
+}
